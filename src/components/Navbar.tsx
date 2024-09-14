0818d42b1e6470722ceafa365cb6c598632af319
shivi028 <tiwarishivi028@gmail.com>
2024-09-14 23:36:55 +0530

    Refactor Components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,42 +5,42 @@ import { cn } from "@/utils/cn";
 import Link from "next/link";
 
 function Navbar({ className }: { className?: string }) {
-    const [active, setActive] = useState<string | null>(null);
-    return (
-        <div
-            className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
-            <Menu setActive={setActive}>
-                <Link href={'/'}>
-                    <MenuItem setActive={setActive} active={active} item="Home">
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+      <Menu setActive={setActive}>
+        <Link href={'/'}>
+          <MenuItem setActive={setActive} active={active} item="Home">
 
-                    </MenuItem>
-                </Link>
+          </MenuItem>
+        </Link>
 
-                <MenuItem setActive={setActive} active={active} item="Our Courses">
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">All Courses</HoveredLink>
-                    </div>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-                    </div>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">Adanced Composition</HoveredLink>
-                    </div>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">Song writing</HoveredLink>
-                    </div>
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/courses">Music Prodection</HoveredLink>
-                    </div>
-                </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+          </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+          </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Adanced Composition</HoveredLink>
+          </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Song writing</HoveredLink>
+          </div>
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">Music Prodection</HoveredLink>
+          </div>
+        </MenuItem>
 
-                <Link href={'/contact'}>
-                    <MenuItem setActive={setActive} active={active} item="Contact Us">
-                    </MenuItem>
-                </Link>
-            </Menu>
-        </div>
-    )
+        <Link href={'/contact'}>
+          <MenuItem setActive={setActive} active={active} item="Contact Us">
+          </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  )
 }
 
 export default Navbar
--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,62 +1,62 @@
 "use client"
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 function MusicSchoolTestimonialCards() {
-    const musicschooltestimonials = [
-        {
-            quote: "Music is the universal language of mankind.",
-            name: "Henry Wadsworth Longfellow",
-            title: "American Poet"
-        },
-        {
-            quote: "Where words fail, music speaks.",
-            name: "Hans Christian Andersen",
-            title: "Danish Author"
-        },
-        {
-            quote: "Without music, life would be a mistake.",
-            name: "Friedrich Nietzsche",
-            title: "German Philosopher"
-        },
-        {
-            quote: "Music can change the world because it can change people.",
-            name: "Bono",
-            title: "Lead Singer of U2"
-        },
-        {
-            quote: "One good thing about music, when it hits you, you feel no pain.",
-            name: "Bob Marley",
-            title: "Reggae Musician"
-        },
-        {
-            quote: "Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.",
-            name: "Plato",
-            title: "Greek Philosopher"
-        },
-        {
-            quote: "Music is the divine way to tell beautiful, poetic things to the heart.",
-            name: "Pablo Casals",
-            title: "Cellist and Conductor"
-        },
-        {
-            quote: "Music is the literature of the heart; it commences where speech ends.",
-            name: "Alphonse de Lamartine",
-            title: "French Writer"
-        },]
-    return (
-        <div className="h-[20rem] w-full  dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center overflow-hidden">
-            <h2 className="text-white flex justify-center mt-5 text-2xl font-semibold z-10 text-center">Hear Our harmony: Voices of Success</h2>
-            <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
-                <div className="w-full max-w-6xl"> 
-                    <InfiniteMovingCards
-                        items={musicschooltestimonials}
-                        direction="right"
-                        speed="slow"
-                    />
-                </div>
-
-            </div>
+  const musicschooltestimonials = [
+    {
+      quote: "Music is the universal language of mankind.",
+      name: "Henry Wadsworth Longfellow",
+      title: "American Poet"
+    },
+    {
+      quote: "Where words fail, music speaks.",
+      name: "Hans Christian Andersen",
+      title: "Danish Author"
+    },
+    {
+      quote: "Without music, life would be a mistake.",
+      name: "Friedrich Nietzsche",
+      title: "German Philosopher"
+    },
+    {
+      quote: "Music can change the world because it can change people.",
+      name: "Bono",
+      title: "Lead Singer of U2"
+    },
+    {
+      quote: "One good thing about music, when it hits you, you feel no pain.",
+      name: "Bob Marley",
+      title: "Reggae Musician"
+    },
+    {
+      quote: "Music gives a soul to the universe, wings to the mind, flight to the imagination, and life to everything.",
+      name: "Plato",
+      title: "Greek Philosopher"
+    },
+    {
+      quote: "Music is the divine way to tell beautiful, poetic things to the heart.",
+      name: "Pablo Casals",
+      title: "Cellist and Conductor"
+    },
+    {
+      quote: "Music is the literature of the heart; it commences where speech ends.",
+      name: "Alphonse de Lamartine",
+      title: "French Writer"
+    },]
+  return (
+    <div className="h-[20rem] w-full  dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center overflow-hidden">
+      <h2 className="text-white flex justify-center mt-5 text-2xl font-semibold z-10 text-center">Hear Our harmony: Voices of Success</h2>
+      <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-6xl">
+          <InfiniteMovingCards
+            items={musicschooltestimonials}
+            direction="right"
+            speed="slow"
+          />
         </div>
-    )
+
+      </div>
+    </div>
+  )
 }
 
 export default MusicSchoolTestimonialCards
